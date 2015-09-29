cc8278fea3e0f366af9dcbeac8d408964fe42cdf
Wilson Rondini <wilbecoding@gmail.com>
2015-09-29 14:34:30 -0600

    Set up env

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var sass = require('node-sass');
+require('dotenv').load();
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
